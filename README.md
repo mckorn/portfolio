cf0382effb575d5f4e237e71737eef31118e1a1e
McKenna Corn <77704171+mckorn@users.noreply.github.com>
2024-11-29 18:54:27 -0500

    updated tab icon and bio description

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## McKenna Corn's Portfolio
+## McKenna Corn's [Portfolio](https://mckenna-corn-2.netlify.app/)
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/91ede682-c8e0-4f2c-9e94-bf0ac3eb3514/deploy-status)](https://app.netlify.com/sites/mckenna-corn-2/deploys)
 
--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -2,8 +2,6 @@ import styles from "./HeroStyles.module.css";
 import heroImg from "../../assets/design-face.png";
 import sun from "../../assets/sun.svg";
 import moon from "../../assets/moon.svg";
-import twitterLight from "../../assets/twitter-light.svg";
-import twitterDark from "../../assets/twitter-dark.svg";
 import linkedinLight from "../../assets/linkedin-light.svg";
 import linkedinDark from "../../assets/linkedin-dark.svg";
 import githubLight from "../../assets/github-light.svg";
@@ -15,7 +13,6 @@ function Hero() {
   const { theme, toggleTheme } = useTheme();
 
   const themeIcon = theme === "light" ? sun : moon;
-  const twitterIcon = theme === "light" ? twitterLight : twitterDark;
   const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
   const githubIcon = theme === "light" ? githubLight : githubDark;
 
@@ -40,21 +37,19 @@ function Hero() {
           <br />
           Corn
         </h1>
-        <h2>Frontend Developer</h2>
+        <h2>Software Developer</h2>
         <span>
-          <a href="https://twitter.com/" target="_blank">
-            <img src={twitterIcon} alt="Twitter icon" />
-          </a>
-          <a href="https://linkedin.com/" target="_blank">
+          <a href="https://linkedin.com/in/mckenna-corn" target="_blank">
             <img src={linkedinIcon} alt="LinkedIn icon" />
           </a>
-          <a href="https://github.com/" target="_blank">
+          <a href="https://github.com/mckorn" target="_blank">
             <img src={githubIcon} alt="Github icon" />
           </a>
         </span>
         <p className={styles.description}>
-          With a passion for developing modern React web apps for commercial
-          businesses.
+          Driven by a passion for crafting unique, user-focused web designs,
+          with experience in building modern React web applications and a strong
+          background in software engineering.
         </p>
         <a href={CV} download>
           <button className="hover">Resume</button>
