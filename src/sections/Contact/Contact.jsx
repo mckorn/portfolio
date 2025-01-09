853a5d7e8ab01a4ab9facd0094237a74bb232a30
McKenna Corn <77704171+mckorn@users.noreply.github.com>
2025-01-09 16:14:00 -0500

    completed customized upgrades

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,10 +1,20 @@
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
 import styles from "./ContactStyles.module.css";
 
 function Contact() {
+  const [state, handleSubmit] = useForm("xannaykg");
+  if (state.succeeded) {
+    return (
+      <p className={styles.success}>
+        Thanks for your message, I'll get back to you as soon as I can!
+      </p>
+    );
+  }
   return (
     <section id="contact" className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
-      <form action="">
+      <form id="contact-form" onSubmit={handleSubmit}>
         <div className="formGroup">
           <label htmlFor="name" hidden>
             Name
@@ -16,31 +26,39 @@ function Contact() {
             placeholder="Name"
             required
           />
+          <ValidationError prefix="Name" field="name" errors={state.errors} />
         </div>
         <div className="formGroup">
           <label htmlFor="email" hidden>
             Email
           </label>
           <input
-            type="text"
+            type="email"
             name="email"
             id="email"
             placeholder="Email"
             required
           />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
         </div>
         <div className="formGroup">
           <label htmlFor="message" hidden>
             Message
           </label>
           <textarea
-            type="message"
+            name="message"
             id="message"
             placeholder="Message"
             required
           ></textarea>
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
         </div>
-        <input className="hover btn" type="submit" value="Submit" />
+        <input
+          className="hover btn"
+          type="submit"
+          value="Submit"
+          disabled={state.submitting}
+        />
       </form>
     </section>
   );
